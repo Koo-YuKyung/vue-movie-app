8cdeb7020274ce4e21d04d8c99ef345d20b975c0
Koo-YuKyung <grayzone13@nate.com>
2021-08-29 04:17:18 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'Koo-Yu',
+    email: 'grayzone13@nate.com',
+    site: 'http://grayzone.site/',
+    phone: '010-9969-****',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
